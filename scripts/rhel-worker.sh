6028c09f32576d45eb73953f35ba06199cdafa83
William Caban <william.caban@redhat.com>
2019-07-12 11:26:43 -0400

    Adding tuned profile

--- scripts/rhel-worker.sh
+++ scripts/rhel-worker.sh
@@ -105,7 +105,22 @@ enroll_and_install_node() {
         # NOTE: Commenting becaue using local repo
         subscription-manager repos --enable=rhel-8-for-x86_64-rt-rpms
         yum groupinstall -y RT
+        set_rt_tuned_profile
     fi
+}
+
+set_rt_tuned_profile() {
+    cat <<EOL > /etc/tuned/realtime-variables.conf
+isolated_cores=2-39
+hugepage_size_default=1G
+hugepage_size=1G
+hugepage_num=32
+EOL
+
+    cmdline_realtime="+isolcpus=\${isolated_cores} intel_pstate=disable nosoftlockup nmi_watchdog=0 audit=0 mce=off kthread_cpus=0 irqaffinity=0 skew_tick=1 processor.max_cstate=1 idle=poll intel_idle.max_cstate=0 intel_pstate=disable intel_iommu=off default_hugepagesz=\${hugepage_size_default} hugepagesz=\${hugepage_size} hugepages=\${hugepage_num} nohz=on nohz_full=\${isolated_cores} rcu_nocbs=\${isolated_cores}"
+
+    sed -i 's|^cmdline_realtime.*|cmdline_realtime='"${cmdline_realtime}"'|' /usr/lib/tuned/realtime/tuned.conf  
+
 }
 
 setup_ignition_service(){    
