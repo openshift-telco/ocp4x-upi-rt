aa843fab459956d160ba8ae9ba9f22240033456d
William Caban <william.caban@redhat.com>
2019-07-12 11:28:50 -0400

    removing _attic for files no longer used

--- _attic/kickstart/scripts/add_rhel8_rt_kernel.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-subscription-manager repos --enable rhel-8-for-x86_64-rt-rpms
-yum groupinstall -y RT
--- _attic/terraform/cluster/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "master_ips" {
-  value = ["${local.master_public_ipv4}"]
-}
-
-output "bootstrap_ip" {
-  value = "${local.bootstrap_public_ipv4}"
-}
-
