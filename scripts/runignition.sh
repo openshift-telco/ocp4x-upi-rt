ccebb4f0d8c74c902b10a2d713d1b19a51f5a3d4
William Caban <william.caban@redhat.com>
2019-07-18 15:23:15 -0400

    Update to call out Nodes CIDS when using proxy

--- scripts/runignition.sh
+++ scripts/runignition.sh
@@ -4,15 +4,15 @@ if [ -e /tmp/runonce ]; then
 
     # run release image
     CLUSTER_VERSION=$(oc get clusterversion --config=/root/.kube/config --output=jsonpath='{.items[0].status.desired.image}')
-    podman pull --tls-verify=false --authfile /tmp/pull.json $CLUSTER_VERSION
-    RELEASE_IMAGE=$(podman run --rm $CLUSTER_VERSION image machine-config-daemon)
+    podman pull --tls-verify=false --authfile /tmp/pull.json ${CLUSTER_VERSION}
+    RELEASE_IMAGE=$(podman run --rm ${CLUSTER_VERSION} image machine-config-daemon)
 
     echo "Using CLUSTER_VERSION=${CLUSTER_VERSION}" >> /root/runignition.log 2>&1
     echo "Using RELEASE_IMAGE=${RELEASE_IMAGE}" >> /root/runignition.log 2>&1
 
     # run MCD image
-    podman pull --tls-verify=false --authfile /tmp/pull.json $RELEASE_IMAGE  >> /root/runignition.log 2>&1
-    podman run -v /:/rootfs -v /var/run/dbus:/var/run/dbus -v /run/systemd:/run/systemd --privileged --rm -ti $RELEASE_IMAGE start --node-name $HOSTNAME --once-from /tmp/bootstrap.ign --skip-reboot >> /root/runignition.log 2>&1
+    podman pull --tls-verify=false --authfile /tmp/pull.json ${RELEASE_IMAGE}  >> /root/runignition.log 2>&1
+    podman run -v /:/rootfs -v /var/run/dbus:/var/run/dbus -v /run/systemd:/run/systemd --privileged --rm -ti ${RELEASE_IMAGE} start --node-name $HOSTNAME --once-from /tmp/bootstrap.ign --skip-reboot >> /root/runignition.log 2>&1
 
     echo "Running MCD completed!" >> /root/runignition.log 2>&1
     systemctl daemon-reload
